09cbc12f016722b93cd9fdce473fd5b9c5196137
jarhuba <jarhuba@gmail.com>
2018-10-28 13:45:52 +0100

    homework 6.3 v.2 some rework

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
@@ -2,19 +2,23 @@ package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
 
+
 public class OddNumberExterminator {
 
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
-        if (numbers.size() == 0) {
-            System.out.println("pusty");
-            return numbers;
+        ArrayList<Integer> listToEdit = numbers;
+        if (listToEdit.size() == 0) {
+            System.out.println("Pusta tablica");
+            listToEdit.add(0); // return only zero if list is empty as a desired result
+            return listToEdit;
         }
 
         for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) % 2 != 0) {
-                numbers.remove(i);
+            if (listToEdit.get(i) % 2 != 0) {
+                listToEdit.remove(i);
+                i = -1;
             }
         }
-        return numbers;
+        return listToEdit;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,11 +1,8 @@
 package com.kodilla.testing.collection;
 
-import com.kodilla.testing.collection.OddNumberExterminator;
 import org.junit.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class CollectionTestSuite {
 
@@ -24,9 +21,9 @@ public class CollectionTestSuite {
         //Given
         OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
         //When
-        ArrayList<Integer> numbers = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>(); //empty list
         ArrayList<Integer> listAfterOddExtermination = oddNumberExterminator.exterminate(numbers);
-        ArrayList<Integer> numbersWithoutOdd = new ArrayList<>(Arrays.asList(0,2,4,6,8,10,12));
+        ArrayList<Integer> numbersWithoutOdd = new ArrayList<>(Arrays.asList(0)); // zero is expected
         System.out.println("Testing Case: " + listAfterOddExtermination);
         //Then
         Assert.assertEquals(numbersWithoutOdd, listAfterOddExtermination);
@@ -37,11 +34,11 @@ public class CollectionTestSuite {
         //Given
         OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
         //When
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9,10,11,12));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0,1,2,3,3,3,4,5,6,7,8,9,10,11,12));
         ArrayList<Integer> listAfterOddExtermination = oddNumberExterminator.exterminate(numbers);
-        ArrayList<Integer> numbersWithoutOdd = new ArrayList<>(Arrays.asList(0,2,4,6,8,10,12));
+        ArrayList<Integer> expectedNumbersWithoutOdd = new ArrayList<>(Arrays.asList(0,2,4,6,8,10,12));
         System.out.println("Testing Case: " + listAfterOddExtermination);
         //Then
-        Assert.assertEquals(numbersWithoutOdd, listAfterOddExtermination);
+        Assert.assertEquals(expectedNumbersWithoutOdd, listAfterOddExtermination);
     }
 }
