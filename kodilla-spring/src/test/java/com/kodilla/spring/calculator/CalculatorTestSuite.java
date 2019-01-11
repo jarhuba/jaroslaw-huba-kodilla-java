33c5e67838a986f2291c916e1be110a5a91c566a
jarhuba <jarhuba@gmail.com>
2019-01-11 22:31:40 +0100

    homework 14.2 Kalkulator z wyświetlaczem

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //When
+        double resultAdd = calculator.add(3, 5);
+        double resultSub = calculator.sub(3, 5);
+        double resultMul = calculator.mul(3, 5);
+        double resultDiv = calculator.div(3, 5);
+
+        //Then
+        Assert.assertEquals(8, resultAdd, 0);
+        Assert.assertEquals(-2, resultSub, 0);
+        Assert.assertEquals(15, resultMul, 0);
+        Assert.assertEquals(0.6, resultDiv, 0.00001);
+    }
+}
