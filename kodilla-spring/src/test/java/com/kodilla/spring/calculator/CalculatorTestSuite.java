ba308820b0ca942640c41171d98c2cae5cc14614
jarhuba <jarhuba@gmail.com>
2019-01-18 21:19:23 +0100

    homework 14.2 add tests

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -23,11 +23,21 @@ public class CalculatorTestSuite {
         double resultSub = calculator.sub(3, 5);
         double resultMul = calculator.mul(3, 5);
         double resultDiv = calculator.div(3, 5);
+        double resultMulZero = calculator.mul(3, 0);
+        double resultDivZero = calculator.div(3, 0);
+        double resultDivZeroMinus = calculator.div(-3, 0);
+        double resultAddMinus = calculator.add(-3, -5);
+        double resultSubMinus = calculator.sub(-3, -5);
 
         //Then
         Assert.assertEquals(8, resultAdd, 0);
         Assert.assertEquals(-2, resultSub, 0);
         Assert.assertEquals(15, resultMul, 0);
         Assert.assertEquals(0.6, resultDiv, 0.00001);
+        Assert.assertEquals(0, resultMulZero, 0);
+        Assert.assertEquals(Double.POSITIVE_INFINITY, resultDivZero, 0);
+        Assert.assertEquals(Double.NEGATIVE_INFINITY, resultDivZeroMinus, 0);
+        Assert.assertEquals(-8, resultAddMinus, 0);
+        Assert.assertEquals(2, resultSubMinus, 0);
     }
 }
