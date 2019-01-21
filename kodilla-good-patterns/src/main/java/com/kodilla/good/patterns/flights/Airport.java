2838fa24ee56fbc0a9a91a21d5465dabfc8369bc
jarhuba <jarhuba@gmail.com>
2019-01-21 16:22:59 +0100

    homework 13.5 package Flights to flights change

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Airport.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Airport.java
@@ -1,4 +1,4 @@
-package com.kodilla.good.patterns.Flights;
+package com.kodilla.good.patterns.flights;
 
 public enum Airport {
     WAW("Warszawa"), GDN("Gdańsk"), KRK("Kraków"), KAT("Katowice"), WRO("Wrocław");
