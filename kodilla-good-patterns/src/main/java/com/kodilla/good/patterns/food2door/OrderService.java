e0bbbae5b8bf9e2880d682a280e097330ab9bf8b
jarhuba <jarhuba@gmail.com>
2019-01-26 12:42:00 +0100

    homework 13.4 next fix

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/OrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/OrderService.java
@@ -1,11 +1,11 @@
 package com.kodilla.good.patterns.food2door;
 
 import java.util.*;
+import java.util.concurrent.atomic.DoubleAdder;
 
 public final class OrderService {
 
     private final InformationService informationService;
-    private double sumToPay = 0.0;
 
     public OrderService(final InformationService informationService) {
         this.informationService = informationService;
@@ -15,27 +15,24 @@ public final class OrderService {
         informationService.sendConfirmationBegin();
 
         Map<Product, Integer> productListDemanded = order.getOrderList();
-        Map<Product, Integer> productListBuyed = new HashMap<>();
+        Map<Product, Integer> productListBought = new HashMap<>();
         Map<Product, Integer> productListMissed = new HashMap<>();
-        //double sumToPay = 0.0;
+        DoubleAdder sumToPay = new DoubleAdder();
 
         System.out.println("Zamawiam: ");
-
         productListDemanded.entrySet().stream()
                 .sorted(Comparator.comparing(e -> e.getKey().getProducer().getProducerName()))
-                .filter(e -> {
+                .forEach(e -> {
                     if (e.getKey().getProducer().process()) {
-                        sumToPay += e.getKey().getProductPrice() * e.getValue();
+                        sumToPay.add(e.getKey().getProductPrice() * e.getValue());
                         System.out.println("Zamówiono produkt: " + e.getKey().getProductName() + " w ilości " + e.getValue() + "\n");
-                        return true;
+                        productListBought.put(e.getKey(), e.getValue());
                     } else {
                         System.out.println("Brak produktu: " + e.getKey().getProductName() + " w ilości " + e.getValue());
                         productListMissed.put(e.getKey(), e.getValue());
-                        return false;
                     }
-                })
-                .forEach(e -> productListBuyed.put(e.getKey(), e.getValue()));
-        System.out.println("Do zapłaty " + sumToPay);
-        informationService.sendConfirmationEnd(productListBuyed, productListMissed);
+                });
+        System.out.println("Do zapłaty " + sumToPay.doubleValue());
+        informationService.sendConfirmationEnd(productListBought, productListMissed);
     }
 }
