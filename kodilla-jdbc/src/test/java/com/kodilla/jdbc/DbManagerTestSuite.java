7d888242c3e8e9d0bc1b2b49a2bdd367610d18ba
jarhuba <jarhuba@gmail.com>
2019-02-08 15:03:25 +0100

    homework 16.6 Zadanie: aktywni użytkownicy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.jdbc;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,14 +9,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DbManagerTestSuite {
+
+    private DbManager dbManager;
+
+    @After
+    public void prepareDbManager() throws SQLException {
+        dbManager = DbManager.getInstance();
+    }
+
     @Test
     public void testConnect() throws SQLException {
         //Given
         //When
-        DbManager dbManager = DbManager.getInstance();
+        DbManager dbManagerTest = DbManager.getInstance();
 
         //Then
-        Assert.assertNotNull(dbManager.getConnection());
+        Assert.assertNotNull(dbManagerTest.getConnection());
     }
 
     @Test
@@ -39,5 +48,26 @@ public class DbManagerTestSuite {
         rs.close();
         statement.close();
         Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "select U.FIRSTNAME, U.LASTNAME from POSTS P join USERS U on P.USER_ID = U.ID group by U.ID having count(*) > 1;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + " " + rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(counter, 1);
     }
 }
