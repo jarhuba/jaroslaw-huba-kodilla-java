3bf7dff5b944c48fa0c955b16e3969657cbdb561
jarhuba@gmail.com <Dragon1Gandalf1>
2019-05-24 14:20:20 +0200

    fixes for kodilla-hibernate

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,15 +1,12 @@
 package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class KodillaHibernateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaHibernateApplication.class, args);
     }
-
 }
